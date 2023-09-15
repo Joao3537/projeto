c267609cad1547c3d46a7dc3f933c7829ae90171
Joao3537 <joao3537@gmail.com>
2023-09-14 21:39:03 -0300

    alteração

--- cadastro.js
+++ cadastro.js
@@ -11,19 +11,19 @@ function verificaDados() {
 
     let caractereVerificaEmail = '@';
 
-    if (usuario <= 5){
-        alert('Campo usuario deve ter mais que 5 caracteres!');
-    }
-    
+   if (usuario.length <= 5 || senha.length < 8){
+        alert('usuário ou senha insuficentes')
+   }
+   else if (usuario.length === "" || senha.length === ""){
+       alert('deve informar os dados do cadastro')
+   }
+
+    //email
     let verificacao = email.match(caractereVerificaEmail);
-    if (verificacao.length <= 0){ //verifica o array de string e checa se há @
+    if (verificacao.length <= 0) {
+        //verifica o array de string e checa se há @
         alert('Campo email deve conter email válido!');
     }
 
-    if (senha.length() < 8 || senha == 'asd'){
-        alert("A senha deve ter mais que 8 caracteres e não pode ser 'asd'");
-    }
-    if (usuario <= "" || verificacao.length <= "" || senha.length() < ""|| senha == ""){
-        alert("deve informar os dados de cadastro");
-    }
+    
 }

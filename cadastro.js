ead0968d239da9925e89b7f1dbbc1b7fe1bbaa52
Joao3537 <joao3537@gmail.com>
2023-09-18 21:21:24 -0300

    commit

--- cadastro.js
+++ cadastro.js
@@ -5,34 +5,36 @@ function verificaDados() {
     //alert(document.getElementById('senha').value)
 
     //capturar info do nome
-    // let usuario = document.getElementById('usuario').value;
-    // let email = document.getElementById('email').value;
-    // let senha = document.getElementById('senha').value;
+     let usuario = document.getElementById('usuario').value;
+     let email = document.getElementById('email').value;
+     let senha = document.getElementById('senha').value;
 
-    // let caractereVerificaEmail = '@';
-    // let verificacao = email.match(caractereVerificaEmail);
+     let caractereVerificaEmail = '@';
+     let verificacao = email.match(caractereVerificaEmail);
 
-    // if (usuario.length <= 5 || senha.length < 8)// usuario/senha
-    // {
-    //     alert('usuário ou senha insuficentes');
-    //     return;
-    // }
-    // else if (usuario.length === "" || senha.length === "")// usuario/senha
-    // {
-    //     alert('deve informar os dados do cadastro');
-    //     return;
-    // }
-    // else if (verificacao.length <= 0)// email
-    // {
-    //     //verifica o array de string e checa se há @
-    //     alert('Campo email deve conter email válido!');
-    //     return;
-    // }
+     if (usuario.length <= 5 || senha.length < 6)// usuario/senha
+     {
+         alert('usuário ou senha insuficentes');
+         return;
+     }
+     else if (usuario.length === "" || senha.length === "")// usuario/senha
+     {
+         alert('deve informar os dados do cadastro');
+         return;
+     }
+     else if (verificacao.length <= 0)// email
+     {
+         //verifica o array de string e checa se há @
+         alert('Campo email deve conter email válido!');
+         return;
+     }
     // //todas as validações estão ok
+    localStorage.setItem(1, usuario.valueOf());
+    localStorage.setItem(2, senha.valueOf());
 
     console.log('passou pelas validações');
 
-    window.location.href = "projeto_site\\login\\login.html";
+    window.location.href = "login.html";
     // chamaLogin();
 }
 
--- login.js
+++ login.js
@@ -1,11 +1,26 @@
 function validaLogin() {
+
+   function carregaUsuario() {
+      if (localStorage.getItem(1) != '') {
+         localStorage.getItem(1).valueOf()
+   
+         document.getElementById('usuario')
+      }
+
+      if (localStorage.getItem(2) != ''){
+         localStorage.getItem(1).valueOf()
+   
+         document.getElementById('senha')
+      }
+   }
+   
    var usuario = document.card.usuario.value;
    var password = document.card.password.value;
 
    if (usuario == "" || password == ""){
       alert("deve informar os dados de login");
    }
-   else if (usuario === "joao" || password === "4687"){
+   else if (usuario === "joao3536" || password === "468725Aa"){
       alert("conta válida");
    }
    else {
